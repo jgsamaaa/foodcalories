74fd2d147f5dc1edbdc1ca1a0660855fb24a2317
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-09-27 23:09:13 -0600

    Seraching food by calorie number done. //Next searching any food or recipes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useState } from 'react';
+import MealList from './MealList';
+
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=2bd207f7572842b3bb082bba4fdd1714&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
   return (
-    <div>
-      <h1 className="text-5xl text-center p-20 bg-blue-400 font-bold">
-        Food Calories Calculator
-      </h1>
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
     </div>
   );
 }
